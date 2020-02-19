9c9d99d308b8cdb169f3273b2071f323d5d38aaf
umitori <jon618@126.com>
2020-02-18 23:20:30 -0800

    test

--- logincl.php
+++ logincl.php
@@ -1,5 +1,5 @@
 <?php
-<<<<<<< HEAD
+
             require "userDB.php";
 
              $account	= $_POST['account'];
@@ -8,12 +8,3 @@
               Loginin($account,$password);    //后面可以做成函数
 
 ?>
-=======
-  require"connect.php";
-  
-   $account	= $_POST['account']; 
-   $password= $_POST['password']; 
-   
-    Loginin($account,$password);    
-?>
->>>>>>> 2eeedd45fae017e5ef0cc249e28498c1a5c185f7
