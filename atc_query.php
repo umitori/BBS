f3b8f26c23bf2d36445d6f3640e501a18d990831
leerx <1056343925@qq.com>
2020-03-31 23:11:52 +0800

    query_article

--- atc_query.php
+++ atc_query.php
@@ -1,36 +1,13 @@
-<?php
+<?php //介个是主页的后端处理php页面
 require "./articleDB.php"; 
-LoginCheck($id);   //先检查有木有登陆  
+LoginCheck($id);   //先检查有木有登陆  $id为session
 $pow = $_SESSION['POW'];
-$id = $_SESSION['id'];  
-article_query($pow);
        
 $json = json_encode(array(
         "resultCode"=>200,
         "message"=>"查询成功！",
-        "data"=>$info
+        "data"=>article_query($pow,$id)
     ),JSON_UNESCAPED_UNICODE);        
-echo($json);
-     
-	
-function article_query($pow,$id){ 
-    $conn = new PDO(DB_DSN, DB_USER, DB_PWD);                                             
-    if ($pow==1){
-			$sth = $conn->prepare("select count(*) as total from article order by id desc" ) ;
-			$sth->execute();
-			$info=$sthh->fetchAll();
-		return $info;
-	}
-
-    else {
-	        $sthh = $conn->prepare("select id,title from article where author = :author order by id desc"); 
-	        $sthh->bindParam(':author', $id);
-	        $sthh->execute();
-	        $info=$sthh->fetchAll();
-		return $info;
-	}
-
-}	
-		
+echo($json);    		
 
 ?>
--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
 define('DB_HOST','localhost');//常量，主机名
 define('DB_USER','root');//连接数据库的用户名
+define("DB_NAME","forum");//要连接的数据库名称
 define('DB_PWD','');//连接数据库密码
 define('DB_PORT','3306');//端口号
 define('DB_TYPE','mysql');//数据库的类型
 define('DB_CHARSET','utf8');//数据库的编码格式
-define('DB_DSN',DB_TYPE.":host=".DB_HOST.";charset=".DB_CHARSET);//定义PDO的DSN
+define('DB_DSN',"mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=".DB_CHARSET);//定义PDO的DSN	
 ?>
